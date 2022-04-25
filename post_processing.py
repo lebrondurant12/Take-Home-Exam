c288d42fc651c2219d6706b8d48f093e7f904ed7
lebrondurant12 <78074374+lebrondurant12@users.noreply.github.com>
2022-04-25 18:12:18 +0800

    Add files via upload

---
+++ post_processing.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 21:45:02 2022
+
+@author: howar
+"""
+
+import re
+
+import pandas as pd
+
+
+def parse_price(price_str: str) -> int:
+    if price_str == "" or "--" in price_str or "無" in price_str:
+        return 0
+    return int(re.match(r"^([\d,]+)\w+", price_str).group(1).replace(",", ""))
+
+
+def auto_marking_(df: pd.DataFrame) -> pd.DataFrame:
+    df["mark"] = ""
+    social = (
+        df.title.str.contains("社宅")
+        | df.title.str.contains("社會住宅")
+        | df.desc.str.contains("社會住宅")
+    )
+    df.loc[social, "mark"] = "x"
+    df.loc[df["提供設備"].str.contains("機械車位"), "mark"] = "x"
+    return df
+
+
+def adjust_price_(df: pd.DataFrame) -> pd.DataFrame:
+    df["price_adjusted"] = (
+        df["price"] * (df["poster"].str.contains("收取服務費").astype("float") * 1 / 24 + 1)
+    ).astype("int")
+    df["price_adjusted"] = df["price_adjusted"] + df["管理費"].fillna("").apply(
+        parse_price
+    )
+    df["price_adjusted"] = (
+        df["price_adjusted"]
+        + df["車位費"].fillna("").str.contains("費用另計").astype("int") * 2500
+    )
+    return df
